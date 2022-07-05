a809147df75d238588e1e34ee57c8cb9e31df463
Jenil D Gohel <j.d.gohel4200@gmail.com>
2022-07-05 10:16:59 +0530

    Json To Data Done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_application_1/widgets/item_widget.dart';
 
 import '../models/catalog.dart';
 import '../widgets/drawer.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalog_dataJson =
+        await rootBundle.loadString("assets/files/catalog_data.json");
+    final decodedData = jsonDecode(catalog_dataJson);
+
+    var productData = decodedData["Products"];
+
+    print(productData);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
