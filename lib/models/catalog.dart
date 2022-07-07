ad028c18f25bfed859527464387bb3c6b8cc1da8
Jenil D Gohel <j.d.gohel4200@gmail.com>
2022-07-07 19:49:19 +0530

    Day 11 Completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,16 +1,8 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 class CatalogModel {
-  static List<Item> items = [
-    Item(
-      itemId: 1,
-      itemName: "iPhone 13 Pro",
-      itemDesc:
-          "A dramatically more powerful camera system. A display so responsive, every interaction feels new again. The world’s fastest smartphone chip. Exceptional durability. And a huge leap in battery life.",
-      itemColor: "#505F4E",
-      itemImage:
-          "https://cdn.mos.cms.futurecdn.net/CAZ6JXi6huSuN4QGE627NR-320-80.jpg",
-      itemPrice: 124000,
-    ),
-  ];
+  static List<Item> items = [];
 }
 
 class Item {
@@ -20,32 +12,85 @@ class Item {
   final num itemPrice;
   final String itemColor;
   final String itemImage;
+  Item({
+    required this.itemId,
+    required this.itemName,
+    required this.itemDesc,
+    required this.itemPrice,
+    required this.itemColor,
+    required this.itemImage,
+  });
 
-  Item(
-      {required this.itemId,
-      required this.itemName,
-      required this.itemDesc,
-      required this.itemPrice,
-      required this.itemColor,
-      required this.itemImage});
-
-  factory Item.fromMap(Map<String, dynamic> map) {
+  Item copyWith({
+    num? itemId,
+    String? itemName,
+    String? itemDesc,
+    num? itemPrice,
+    String? itemColor,
+    String? itemImage,
+  }) {
     return Item(
-      itemId: map["itemId"],
-      itemName: map["itemName"],
-      itemDesc: map["itemDesc"],
-      itemPrice: map["itemPrice"],
-      itemColor: map["itemColor"],
-      itemImage: map["itemImage"],
+      itemId: itemId ?? this.itemId,
+      itemName: itemName ?? this.itemName,
+      itemDesc: itemDesc ?? this.itemDesc,
+      itemPrice: itemPrice ?? this.itemPrice,
+      itemColor: itemColor ?? this.itemColor,
+      itemImage: itemImage ?? this.itemImage,
     );
   }
 
-  toMap() => {
-        "itemId": itemId,
-        "itemName": itemName,
-        "itemDesc": itemDesc,
-        "itemPrice": itemPrice,
-        "itemColor": itemColor,
-        "itemImage": itemImage,
-      };
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'itemId': itemId,
+      'itemName': itemName,
+      'itemDesc': itemDesc,
+      'itemPrice': itemPrice,
+      'itemColor': itemColor,
+      'itemImage': itemImage,
+    };
+  }
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      itemId: map['itemId'] as num,
+      itemName: map['itemName'] as String,
+      itemDesc: map['itemDesc'] as String,
+      itemPrice: map['itemPrice'] as num,
+      itemColor: map['itemColor'] as String,
+      itemImage: map['itemImage'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Item.fromJson(String source) =>
+      Item.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'Item(itemId: $itemId, itemName: $itemName, itemDesc: $itemDesc, itemPrice: $itemPrice, itemColor: $itemColor, itemImage: $itemImage)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Item &&
+        other.itemId == itemId &&
+        other.itemName == itemName &&
+        other.itemDesc == itemDesc &&
+        other.itemPrice == itemPrice &&
+        other.itemColor == itemColor &&
+        other.itemImage == itemImage;
+  }
+
+  @override
+  int get hashCode {
+    return itemId.hashCode ^
+        itemName.hashCode ^
+        itemDesc.hashCode ^
+        itemPrice.hashCode ^
+        itemColor.hashCode ^
+        itemImage.hashCode;
+  }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
 import 'package:flutter_application_1/widgets/item_widget.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../models/catalog.dart';
 import '../widgets/drawer.dart';
@@ -14,12 +15,14 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   void initState() {
+    // ignore: todo
     // TODO: implement initState
     super.initState();
     loadData();
   }
 
   loadData() async {
+    await Future.delayed(Duration(seconds: 2));
     final catalog_dataJson =
         await rootBundle.loadString("assets/files/catalog_data.json");
     final decodedData = jsonDecode(catalog_dataJson);
@@ -39,12 +42,55 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: CatalogModel.items.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(item: CatalogModel.items[index]);
-          },
-        ),
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 7,
+                ),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    child: GridTile(
+                      header: Container(
+                        child: Text(
+                          item.itemName,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: GoogleFonts.lato().fontFamily,
+                          ),
+                        ),
+                        padding: const EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurpleAccent,
+                        ),
+                      ),
+                      child: Image.network(
+                        item.itemImage,
+                        fit: BoxFit.cover,
+                      ),
+                      footer: Container(
+                        child: Text(
+                          item.itemPrice.toString(),
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: GoogleFonts.lato().fontFamily,
+                          ),
+                        ),
+                        padding: const EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                itemCount: CatalogModel.items.length)
+            : Center(child: CircularProgressIndicator()),
       ),
       drawer: MyDrawer(),
     );
